3c79eee83ad2c6d6fdff5c5e606794ef91da427c
sebastianlloves <sebastianlloves@gmail.com>
2023-07-26 01:36:08 -0300

    .

---
+++ e-commerce/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MiComponente from '../src/Components/MiComponente'
+
+function App() {
+  return (
+    <div className="App">
+      <MiComponente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce/src/Components/MiComponente.js
@@ -0,0 +1,14 @@
+import logo from "../logo.svg";
+
+const MiComponente = () => {
+  return (
+    <div>
+      <h2>Mi primer componente</h2>
+      <p>En REACT JS</p>
+      <img
+        src={logo} alt="jasdj" width="200px"/>
+    </div>
+  );
+};
+
+export default MiComponente;
