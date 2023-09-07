7d94c777f0f5a7201e5a5e33915adc6c88aec83a
sebastianlloves <sebastianlloves@gmail.com>
2023-09-07 16:05:45 -0300

    .

--- ProyectoIntegrador/src/Components/Products/ProductDetail.jsx
+++ ProyectoIntegrador/src/Components/Products/ProductDetail.jsx
@@ -173,7 +173,7 @@ function BotonComprar({
     <div>
       <button
         type="submit"
-        className="mx-auto mt-20 flex w-1/2 min-w-fit items-center justify-center rounded-md border border-transparent bg-sky-800 px-8 py-3 text-base font-medium text-white shadow-sm shadow-sky-300 duration-300 hover:bg-sky-900 active:-translate-y-0.5 active:shadow-md active:ring-1  active:ring-sky-700 active:ring-offset-2 lg:w-full"
+        className="mx-auto mt-20 flex w-1/2 min-w-fit items-center justify-center rounded-md border border-transparent bg-sky-800 px-8 py-3 text-base font-medium text-white shadow-md shadow-sky-200 duration-300 hover:bg-sky-900 active:-translate-y-0.5 active:shadow-lg active:ring-1  active:ring-sky-700 active:ring-offset-2 lg:w-full"
         onClick={handleClick}
       >
         Agregar al carrito
