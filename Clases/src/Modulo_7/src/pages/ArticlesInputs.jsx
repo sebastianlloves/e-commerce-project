ed64f47be344884b94b3015ed6c8843524032f10
sebastianlloves <sebastianlloves@gmail.com>
2023-10-27 00:15:33 -0300

    .

--- Clases/src/Modulo_7/src/pages/ArticlesInputs.jsx
+++ Clases/src/Modulo_7/src/pages/ArticlesInputs.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
 
 const ArticlesInputs = () => {
-  const [id, setId] = useState("");
-  const [description, setDescription] = useState("");
-  const [stock, setStock] = useState("");
-  const [price, setPrice] = useState("");
+  const [formData, setFormData] = useState({
+    id: "",
+    description: "",
+    stock: "",
+    price: "",
+  });
+
+  const handleChange = ({target:{name, value}}) => setFormData({...formData, [name]: value})
 
   return (
     <>
-      <form className="flex flex-col space-y-4 p-10 bg-white w-1/3">
+      <form onSubmit={e => {
+        e.preventDefault()
+        console.log(formData)
+      }} className="flex flex-col space-y-4 p-10 bg-white w-1/3">
         <label>Ingrese el ID:</label>
-        <input type="number" name="id" value={id} className="border-gray-400 border-2 bg-white"/>
+        <input type="number" name="id" className="border-gray-400 border-2 bg-white" onChange={handleChange}/>
 
         <label>Ingrese la descripción:</label>
-        <input type="text" name="description" value={description} className="border-gray-400 border-2 bg-white mb-4"/>
+        <input type="text" name="description" className="border-gray-400 border-2 bg-white mb-4" onChange={handleChange}/>
 
         <label>Ingrese el stock:</label>
-        <input type="number" name="stock" value={stock} className="border-gray-400 border-2 bg-white mb-4"/>
+        <input type="number" name="stock" className="border-gray-400 border-2 bg-white mb-4" onChange={handleChange}/>
 
         <label>Ingrese el precio ($):</label>
-        <input type="number" name="price" value={price} className="border-gray-400 border-2 bg-white mb-4"/>
+        <input type="number" name="price" className="border-gray-400 border-2 bg-white mb-4" onChange={handleChange}/>
 
         <button type="submit">CARGAR</button>
       </form>
