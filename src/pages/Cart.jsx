bac27111dae9459e67a16ca04ae58b5c05f5f92d
sebastianlloves <sebastianlloves@gmail.com>
2023-11-21 00:40:26 -0300

    .

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -12,13 +12,18 @@ function Cart() {
   return (
     <div className="min-h-screen w-full bg-white">
       <div className="m-auto flex w-3/4 max-w-screen-lg flex-col py-24">
-        <h2 className="my-10 text-left text-3xl font-semibold uppercase text-slate-800">
-          Mi Carrito
-        </h2>
+        <div className="flex items-center justify-between">
+          <h2 className="my-10 text-left text-3xl font-semibold uppercase text-slate-800">
+            Carrito
+          </h2>
 
-        <Link to="../">
-          <button>Seguir Comprando</button>
-        </Link>
+          <Link
+            to="../"
+            className="self-end rounded-md border border-sky-900/30 px-5 py-2 text-slate-500 hover:shadow-md shadow-sky-300/70 duration-300 font-normal hover:bg-sky-900/75 hover:text-white"
+          >
+            <button>Seguir Comprando</button>
+          </Link>
+        </div>
 
         {cartItems.length > 0 ? <CartList /> : <EmptyCart />}
       </div>
