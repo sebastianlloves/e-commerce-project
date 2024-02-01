0c22b48811ede87c7a1d0d85a35e27a046f54f22
sebastianlloves <sebastianlloves@gmail.com>
2024-02-01 01:23:25 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navigation from "./routes/Navigation";
-import './ResetCss.css'
+import './Styles.css'
 
 export default function App() {
-  useEffect(() => {
-    async () => {
-      const response = await fetch(
-        "https://store.renault.com.ar/?ORIGIN=sea_defensive&CAMPAIGN=ar-nov-2023-search-ads-360-l-newcar-renault_store&gclsrc=aw.ds&&gclid=CjwKCAiAxreqBhAxEiwAfGfndJaBy4LCkBvWFgQDmQtxvGVxQryos5p-2x0rMIK_6cZo4DcLJbx3choCybkQAvD_BwE&pdv=c"
-      );
-      console.log(response.json());
-    };
-  }, []);
-
   return <Navigation />;
 }
