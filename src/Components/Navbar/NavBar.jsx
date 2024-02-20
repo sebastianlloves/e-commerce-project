a8eb5315756afab63d8b902e7884781696c7c70a
sebastianlloves <sebastianlloves@gmail.com>
2024-02-20 19:04:11 -0300

    .

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -15,7 +15,7 @@ export default function NavBar() {
 
   return (
     <header
-      className="scroll-navbar fixed top-0 z-10 flex h-16 w-full flex-col items-start justify-start  bg-azure-950/95 font-navbar text-base font-medium text-slate-300 shadow-[0_7px_9px_-3px_rgba(36,46,71,0.3)] backdrop-blur"
+      className="scroll-navbar fixed top-0 z-10 flex h-16 w-full flex-col items-start justify-start bg-azure-950/95  font-navbar text-base font-medium text-slate-300 shadow-[0_7px_9px_-3px_rgba(36,46,71,0.3)] backdrop-blur lg:items-center"
       style={{ viewTransitionName: "navbar" }}
     >
       <nav className="z-10 flex h-full w-full max-w-7xl items-center justify-between  px-8">
@@ -32,7 +32,7 @@ export default function NavBar() {
         <CartButton className="carrito" />
       </nav>
       <div
-        className={`scroll-navbar flex min-h-screen w-1/3 flex-col items-start justify-start space-y-6 bg-inherit py-6 backdrop-blur-2xl transition-all duration-200 px-2${
+        className={`scroll-navbar [backdrop-filter: blur(15px);] flex min-h-screen w-1/3 flex-col items-start justify-start space-y-6 bg-inherit py-6 backdrop-blur transition-all duration-200 px-2${
           mobileToggleisOpen
             ? " opacity-1 shadow-[0_7px_9px_-3px_rgba(36,46,71,0.3)] "
             : " -translate-x-full opacity-0"
