f573e4dd676761e6d88fde144acf5ffd5ebc3e3c
sebastianlloves <sebastianlloves@gmail.com>
2024-02-21 15:44:07 -0300

    .

--- src/Components/Products/Card.jsx
+++ src/Components/Products/Card.jsx
@@ -4,16 +4,16 @@ import ViewTransitionLink from "../../routes/ViewTransitionLink";
 
 function Card({ product: { id, name, imagesURL, price } }) {
   return (
-    <div
-      id={`prod-${id}`}
-      onClick={() => sessionStorage.setItem("scrollProduct", id)}
-      className="card flex max-w-sm flex-col justify-between rounded-lg border-2 border-transparent p-4 pb-6 shadow-zinc-200 transition-all duration-300 hover:scale-[101%] hover:border-zinc-100 hover:bg-zinc-50 hover:shadow-md"
-    >
-      <ViewTransitionLink to={`/product/${id}`}>
+    <ViewTransitionLink to={`/product/${id}`}>
+      <div
+        id={`prod-${id}`}
+        onClick={() => sessionStorage.setItem("scrollProduct", id)}
+        className="card group flex max-w-sm flex-col justify-between rounded-lg border-2 border-transparent p-4 pb-6 shadow-zinc-200 transition-all duration-300 hover:border-zinc-100 hover:bg-zinc-50 hover:shadow-md"
+      >
         <div className="flex h-60 w-full items-center justify-center overflow-hidden rounded-md border border-stone-100 bg-white p-6 duration-300 group-hover:opacity-90">
           <img
             src={imagesURL[0]}
-            className="img-product h-full object-contain object-center"
+            className="img-product h-full object-contain object-center transition-all duration-700 group-hover:scale-105"
             style={{ viewTransitionName: `img-product-${id}` }}
           />
         </div>
@@ -29,8 +29,8 @@ function Card({ product: { id, name, imagesURL, price } }) {
             // style={{ viewTransitionName: `price-product-${id}` }}
           >{`U$D ${price}`}</p>
         </div>
-      </ViewTransitionLink>
-    </div>
+      </div>
+    </ViewTransitionLink>
   );
 }
 
